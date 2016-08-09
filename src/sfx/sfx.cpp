73c6754d10d533cea2e79f117e8f4e6c257a57f9
Nathan Ogden <nathanogden@chompy.me>
2016-08-09 09:58:25 -0400

    music loading/playing methods not yet tested

--- src/sfx/sfx.cpp
+++ src/sfx/sfx.cpp
@@ -1,7 +1,14 @@
 #include "sfx.h"
 
+char ChompSfx::MUSIC_ASSET_PREFIX[] = "mus_";
+
 ChompSfx::ChompSfx()
 {
+    music = nullptr;
+    queuedMusic = "";
+    queuedMusicOperation = 0;
+    queuedMusicLoops = 0;
+    queuedMusicFadeDuration = 0;
     int flags = MIX_INIT_OGG;
     int initted = Mix_Init(MIX_INIT_OGG);
     if (initted&flags != flags) {
@@ -16,5 +23,123 @@ ChompSfx::ChompSfx()
 
 ChompSfx::~ChompSfx()
 {
+    unloadMusic();
     Mix_Quit();
+}
+
+uint8_t ChompSfx::loadMusic(char* name)
+{
+    return loadMusic(name, MUSIC_STOP, 0, 0);
+}
+
+uint8_t ChompSfx::loadMusic(char* name, uint8_t operation)
+{
+    return loadMusic(name, operation, -1, 0);
+}
+
+uint8_t ChompSfx::loadMusic(char* name, uint8_t operation, int16_t loops)
+{
+    return loadMusic(name, operation, loops, 0);
+}
+
+uint8_t ChompSfx::loadMusic(char* name, uint8_t operation, int16_t loops, int16_t fadeDuration)
+{
+    if (!name || operation == MUSIC_FADEOUT) {
+        return MUSIC_LOAD_FAILED;
+    }
+
+    // queue up music if previous music is fading out
+    if (Mix_FadingMusic() == MIX_FADING_OUT) {
+        queuedMusic = std::string(name);
+        queuedMusicOperation = operation;
+        queuedMusicLoops = loops;
+        queuedMusicFadeDuration = fadeDuration;
+        return MUSIC_LOAD_QUEUED;
+    }
+
+    // build asset name string
+    uint8_t assetPrefixLen = strlen(ChompSfx::MUSIC_ASSET_PREFIX);
+    char assetName[assetPrefixLen + strlen(name) + 1];
+    memcpy(assetName, ChompSfx::MUSIC_ASSET_PREFIX, assetPrefixLen);
+    memcpy(&assetName[assetPrefixLen], name, strlen(name));
+    assetName[assetPrefixLen + strlen(name)] = '\0';
+    // load asset
+    if (!ChompAsset::assetExists(assetName)) {
+        return MUSIC_LOAD_FAILED;
+    }
+    // get filesize
+    uint32_t fileSize = ChompAsset::getAssetSize(assetName);
+    // get data
+    uint8_t musicData[fileSize];
+    ChompAsset::readFile(assetName, 0, &musicData[0], fileSize);
+    SDL_RWops* musicDataRW = SDL_RWFromMem(&musicData[0], fileSize);
+
+    // load music
+    unloadMusic();
+    music = Mix_LoadMUS_RW(musicDataRW, 1);
+    setMusic(operation, loops, fadeDuration);
+    return MUSIC_LOAD_COMPLETE;
+
+}
+
+void ChompSfx::unloadMusic()
+{
+    Mix_HaltMusic();
+    if (music) {
+        Mix_FreeMusic(music);
+        music = nullptr;
+    }
+}
+
+void ChompSfx::checkMusicQueue()
+{
+    if (!queuedMusic.empty() && Mix_FadingMusic() != MIX_FADING_OUT && !Mix_PlayingMusic()) {
+        loadMusic((char*) queuedMusic.c_str(), queuedMusicOperation, queuedMusicLoops, queuedMusicFadeDuration);
+        queuedMusic = "";
+        queuedMusicOperation = 0;
+        queuedMusicLoops = 0;
+        queuedMusicFadeDuration = 0;
+    }
+}
+
+void ChompSfx::setMusic(uint8_t operation)
+{
+    setMusic(operation, -1, 0);
+}
+
+void ChompSfx::setMusic(uint8_t operation, int16_t loops)
+{
+    setMusic(operation, loops, 0);
+}
+
+void ChompSfx::setMusic(uint8_t operation, int16_t loops, int16_t fadeDuration)
+{
+    switch (operation) {
+        case MUSIC_STOP:
+        {
+            Mix_HaltMusic();
+            break;
+        }
+        case MUSIC_PLAY:
+        {
+            if (music) {
+                Mix_PlayMusic(music, loops);
+            }
+            break;
+        }
+        case MUSIC_FADEIN:
+        {
+            if (music) {
+                Mix_FadeInMusic(music, loops, fadeDuration);
+            }
+            break;
+        }
+        case MUSIC_FADEOUT:
+        {
+            if (Mix_PlayingMusic()) {
+                Mix_FadeOutMusic(fadeDuration);
+            }
+            break;
+        }
+    }
 }
--- src/sfx/sfx.h
+++ src/sfx/sfx.h
@@ -5,16 +5,48 @@
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_mixer.h"
 #include "../exception.h"
+#include "../asset/asset.h"
+
+#define MUSIC_STOP 0
+#define MUSIC_PLAY 1
+#define MUSIC_FADEIN 2
+#define MUSIC_FADEOUT 3
+
+#define MUSIC_LOAD_FAILED 0
+#define MUSIC_LOAD_COMPLETE 1
+#define MUSIC_LOAD_QUEUED 2
 
 class ChompSfx {
 
 public:
 
+    static char MUSIC_ASSET_PREFIX[];
+
     ChompSfx();
     ~ChompSfx();
 
-    void loadMusic(char* name);
+    // music (one at a time)
+    uint8_t loadMusic(char* name);
+    uint8_t loadMusic(char* name, uint8_t operation);
+    uint8_t loadMusic(char* name, uint8_t operation, int16_t loops);
+    uint8_t loadMusic(char* name, uint8_t operation, int16_t loops, int16_t fadeDuration);
+    void unloadMusic();
+    void checkMusicQueue();
+    void setMusic(uint8_t operation);
+    void setMusic(uint8_t operation, int16_t loops);
+    void setMusic(uint8_t operation, int16_t loops, int16_t fadeDuration);
+    
+    // sfx
     void loadChunk(char* name);
+
+protected:
+
+    Mix_Music* music;    
+
+    std::string queuedMusic;
+    uint8_t queuedMusicOperation;
+    int16_t queuedMusicLoops;
+    int16_t queuedMusicFadeDuration;
 
 };
 
